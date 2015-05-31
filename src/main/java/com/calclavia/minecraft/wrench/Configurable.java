a8d82bfc4549b16518acdf48f5d28e34777785d8
Calclavia <admin@calclavia.com>
2015-06-01 00:49:08 +0800

    Added Configurable component for configurator items such as wrench or screwdriver

---
+++ src/main/java/com/calclavia/minecraft/wrench/Configurable.java
@@ -0,0 +1,25 @@
+package com.calclavia.minecraft.wrench;
+
+import nova.core.component.Component;
+import nova.core.entity.Entity;
+import nova.core.event.EventBus;
+
+/**
+ * Add to blocks that can be configured (via wrench)
+ * @author Calclavia
+ */
+public class Configurable extends Component {
+
+	/**
+	 * Called when the block is configured by a configurator.
+	 */
+	public EventBus<ConfigureEvent> onConfig = new EventBus<>();
+
+	public static class ConfigureEvent {
+		public final Entity player;
+
+		public ConfigureEvent(Entity player) {
+			this.player = player;
+		}
+	}
+}
